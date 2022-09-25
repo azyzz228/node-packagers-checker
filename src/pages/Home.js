9c428cd4c5d3fea2b436143b674217f48c808416
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-09-25 03:37:41 -0400

    final commit
     I had to reverty the merge and code one component all over again

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,167 +1,147 @@
-import axios from "axios";
-import { useRef, useState } from "react";
-import convert_date from "../helpers/convert_date";
-import package_info_object from "../helpers/package_info_object";
+import React, { useState } from 'react'
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+} from 'recoil';
+import PackageInfo from '../components/PackageInfo'
+
+
+
+//const mappedDependenciesValue = useRecoilValue(mappedDependencies)
 
 function Home() {
-  const [packages, setPackages] = useState([]);
-  const githubLink = useRef()
-  const branch = useRef()
-  let arr = [];
-  let p_arr = [];
-  let github, metadata;
 
-  async function jsonData(url) { // (1)
-    let res = await fetch(url); // (2)
+    //const [dependencies, setDependencies] = useRecoilState(dependenciesState)
 
-    if (res.status == 200) {
-      let json = await res.json(); // (3)
-      return json;
+    const [repos, setRepos] = useState([])
+    const [finished, setFinished] = useState(false)
+    const githubLink = useRef()
+    const branch = useRef()
+
+    let arr = [];
+
+    async function handleSubmit() {
+        setRepos([])
+        setFinished(false)
+        let username, repo;
+        username = githubLink.current.value.split("/")[3];
+        repo = githubLink.current.value.split("/")[4];
+
+
+        const url = `https://raw.githubusercontent.com/${username}/${repo}/${branch.current.value}/package.json`
+        const resp = await fetch(url);
+        if (resp.status !== 200) {
+            alert("Please insert github repo link in right format")
+            return
+        }
+        localStorage.setItem("githubLink", githubLink.current.value)
+        localStorage.setItem("branch", branch.current.value)
+
+
+        const body = await resp.json();
+        //const dependencies_array = Object.keys(body.dependencies)
+        //setDependencies(dependencies_array);
+        Object.keys(body.dependencies).map(async (item) => {
+            if (item.includes("@")) {
+                let p = item.split("/")[0].slice(1)
+                const url = "https://api.npms.io/v2/package/" + p;
+                const resp = await fetch(url);
+                const body = await resp.json()
+                if (!arr.includes(body.collected.metadata.name)) {
+                    arr.push(body.collected.metadata.name);
+                    setRepos(current => [...current, body])
+
+                }
+
+            } else if (!item.includes("@")) {
+                const url = "https://api.npms.io/v2/package/" + item;
+                const resp = await fetch(url);
+                const body = await resp.json();
+                if (!arr.includes(body.collected.metadata.name)) {
+                    arr.push(body.collected.metadata.name);
+                    setRepos(current => [...current, body])
+
+                }
+            }
+        })
+
+        setFinished(true);
+
+    };
+
+
+    const finishLine = async () => {
+        await handleSubmit();
+        const res = await Promise.all(repos);
+        console.log(res);
     }
 
-    throw new Error(res.status);
-  }
-
-  async function dependencies(item) {
-    if (item.includes("@")) {
-      let p = item.split("/")[0].slice(1)
-      if (!p_arr.includes(p)) {
-        return await jsonData(`https://api.npms.io/v2/package/${p}`);
-      }
-    } else if (!item.includes("@")) {
-      return await jsonData(`https://api.npms.io/v2/package/${item}`);
-    }
-  }
-
-  const handleSubmit = async () => {
-    let username, repo;
-    username = githubLink.current.value.split("/")[3];
-    repo = githubLink.current.value.split("/")[4];
-
-    fetch(`https://raw.githubusercontent.com/${username}/${repo}/${branch.current.value}/package.json`)
-      .then(function (response) {
-
-        return response.json()
-      }
-      ).then(res => {
-        let array_dependencies = Object.keys(res.dependencies);
-
-        Promise.all(array_dependencies.map(dependencies)
-        ).then(data => {
-
-          setPackages(data)
-          console.log(data);
-
-        });
+    useEffect(() => {
+        if (localStorage.getItem("githubLink") !== null &&
+            localStorage.getItem("branch") !== null
+        ) {
+            githubLink.current.value = localStorage.getItem("githubLink")
+            branch.current.value = localStorage.getItem("branch")
+            finishLine();
+            return
+        }
+    }, [])
 
 
-      })
 
-  }
 
-  return (
-    <div>
-      <div className="p-20 flex flex-col justify-start items-start gap-12 bg-slate-50">
 
-        <h1 className="text-5xl text-neutral-900 ">Hello</h1>
-        <div className="flex flex-row gap-8">
-          <input type="text" ref={githubLink} className="outline-none border border-blue-200 shadow-md p-2 rounded-lg w-[450px]" name="githubLink" id="" placeholder="https://github.com/azyzz228/hsl-color-picker" />
+    return (
+        <>
 
-          <input type="text" ref={branch} className="outline-none border border-blue-200 shadow-md p-2 rounded-lg w-24" name="branch" id="" placeholder="master" />
+            <div className="p-20 flex flex-col justify-start items-start gap-12 bg-slate-50">
+                <h1 className="text-5xl text-neutral-900 ">Eye of Packages 👁</h1>
+                <p className='text-lg text-neutral-800'>Are you aware of what npm packages you have installed? What do they do? When they were updated? Their issues and dependencies?
+                    <br />
+                    Eye of Packages is a All-Seeing Eye for your npm packages. It is aimed at helping you identify potentially harmful and/or outdated packages
+                </p>
+                <p className='text-neutral-700'>
+                    The project is developed by <a href="https://github.com/azyzz228" target={"_blank"} className="text-blue-700 hover:text-blue-900">Aziz Abdullaev</a> and <a href="https://github.com/Chukwuemeka-Mba" target={"_blank"} className="text-blue-700 hover:text-blue-900">Chukwuemeka Mba</a> for MLH Fellowship Orientation Hackathon, Fall 2022.
+                    Github link is <a href="https://github.com/azyzz228/node-packagers-checker" target={"_blank"} className="text-orange-700 hover:text-orange-900">here</a>
+                </p>
 
-          <button
-            className="p-4 text-blue-100 bg-blue-600 rounded-lg hover:bg-blue-700 active:bg-blue-900"
-            onClick={() => handleSubmit()}
-          >
-            Submit
-          </button>
-        </div>
+                <div className=" container mx-auto flex flex-col items-center mt-8">
+                    <p className='mb-4 text-sm text-neutral-800'>Insert a link to your Github repo with package.json file as well as branch name.</p>
+                    <div className="flex flex-row gap-8">
+                        <input type="text" ref={githubLink} className="outline-none border border-blue-200 shadow-md p-2 rounded-lg w-[450px]" name="githubLink" id="" placeholder="https://github.com/azyzz228/hsl-color-picker" />
 
-      </div>
+                        <input type="text" ref={branch} className="outline-none border border-blue-200 shadow-md p-2 rounded-lg w-24" name="branch" id="" placeholder="master" />
 
-      <h1 className="px-20 text-3xl text-neutral-900 mt-10">List of your packages:</h1>
-      <div className=" px-10 pb-20 pt-10 grid grid-cols-3 gap-12 container mx-auto">
-
-        {packages.length === 0 ? (
-          <p>Its empty</p>
-        ) : (
-          packages.map((item, index) => (
-            <div
-              key={index}
-              class="max-w-3xl rounded-t-lg rounded-b-md border border-t-4 border-slate-100 border-t-blue-400 px-8 py-4 shadow-lg"
-            >
-              <div class="flex flex-row items-center justify-between border-b pb-4">
-                <a href={`/package-dependencies/${item.name}`}>
-                  <h1 class="text-2xl font-semibold text-slate-700 hover:underline">
-                    {item.name}
-                  </h1>
-                </a>
-                <div class="flex flex-col items-end">
-                  <p class="text-sm text-slate-600">Last updated</p>
-                  <p class="text-lg font-bold text-slate-800">{item.date}</p>
+                        <button
+                            className="p-4 text-blue-100 bg-blue-600 rounded-lg hover:bg-blue-700 active:bg-blue-900"
+                            onClick={() => finishLine()}
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </div>
-              </div>
 
-              <p class="border-b border-slate-100 py-4 text-slate-700">
-                {item.description}
-              </p>
-              <p className="text-slate-600 py-4 flex justify-center">
-                Github Stats
-              </p>
-              <div class="flex flex-row items-center justify-between py-4 text-xs text-slate-700" >
-                <p className="flex flex-row items-center gap-1"><i className="fa-solid fa-star text-base text-[#9c7140]"></i><span className="text-yellow-900 text-lg font-semibold">{item.starsCount}</span></p>
-
-                <p className="flex flex-row items-center gap-1"><i className="fa-solid fa-code-commit text-base text-[#9c7140]"></i><span className="text-yellow-900 text-lg font-semibold">{item.forksCount}</span></p>
-
-                <p className="flex flex-row items-center gap-1"><i className="fa-solid fa-user-group text-base text-[#9c7140]"></i><span className="text-yellow-900 text-lg font-semibold">{item.subscribersCount}</span></p>
-              </div>
-
-              <div class="flex flex-row items-center justify-between py-4">
-                <a href={`${item.issues}`} target="_blank">
-
-                  <p class="text-red-900">Issues</p>
-                </a>
-                <p class="text-xl font-bold text-red-700">78</p>
-              </div>
-
-              <p class="pb-3 text-slate-700">Dependencies:</p>
-              <div class="grid grid-cols-2 place-items-start items-center gap-4">
-                {item.dependencies.map((i, k) => {
-
-                  if (i.includes('@')) {
-
-                    return (
-                      <a href={`/package-dependencies/${i.split("/")[0].slice(1)}`} key={k}>
-                        <p
-
-                          class="col-span-1 rounded-full bg-slate-50 py-1 px-4 font-semibold text-slate-700"
-                        >
-                          {i}
-                        </p>
-                      </a>
-                    )
-                  }
-                  else {
-                    return (
-                      <a href={`/package-dependencies/${i}`} key={k}>
-                        <p
-
-                          class="col-span-1 rounded-full bg-slate-50 py-1 px-4 font-semibold text-slate-700"
-                        >
-                          {i}
-                        </p>
-                      </a>
-                    )
-                  }
-
-
-                })}
-              </div>
             </div>
-          ))
-        )}
-      </div>
-    </div >
-  );
+
+            <div className="px-10 pb-20 pt-10 grid md:grid-cols-2 lg:grid-cols-3 gap-12 container mx-auto">
+
+                {finished ? (
+                    repos.map((item, index) => (
+                        <PackageInfo item={item} key={index} />
+                        // <h1 key={index}>{JSON.stringify(item)}</h1>
+                    ))
+                ) : (
+                    <p>Packages you have used will appear here.</p>
+                )}
+
+            </div>
+        </>
+    )
 }
-export default Home;
+
+export default Home
